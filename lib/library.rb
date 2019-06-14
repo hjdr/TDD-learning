2a2597d5f4e99dda828ef305c1c2dba11c10ccdb
hjdr <harry.jdr@gmail.com>
2019-06-14 21:50:42 +0100

    added library.get_books_in_category method & test

--- lib/library.rb
+++ lib/library.rb
@@ -1,5 +1,4 @@
 require 'yaml'
-
 Library = Class.new
 
 class Library
@@ -8,5 +7,11 @@ class Library
   def initialize(lib_file = false)
     @lib_file = lib_file
     @books = @lib_file ? YAML::load(File.read(@lib_file)) : []
+  end
+
+  def get_books_in_category(category)
+    @books.select do |book|
+      book.category == category
+    end
   end
 end
--- lib/spec/library_spec.rb
+++ lib/spec/library_spec.rb
@@ -5,7 +5,11 @@ describe "Library Object" do
 
   before :all do
     lib_arr = [
-      Book.new("testTitle", "testAuthor", :testcat)
+      Book.new("testTitle100", "testAuthor100", :testcat),
+      Book.new("testTitle101", "testAuthor101", :testcat),
+      Book.new("testTitle102", "testAuthor102", :testcat101),
+      Book.new("testTitle103", "testAuthor103", :testcat101),
+      Book.new("testTitle104", "testAuthor104", :testcat101)
     ]
 
     File.open("books.yml", "w") do |file|
@@ -27,8 +31,12 @@ describe "Library Object" do
 
     context "with a yaml name argument" do
       it "has one book" do
-        expect(@lib.books.length).to eq(1)
+        expect(@lib.books.length).to eq(5)
       end
     end
+  end
+
+  it "returns all books in a given category" do
+    expect(@lib.get_books_in_category(:testcat101).length).to eq(3)
   end
 end
